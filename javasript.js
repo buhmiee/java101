44b83a1ad2260b7c9d4f1f90dcb632661725474f
OluwabunmiOladipo <oluwabunmioladipo6@gmail.com>
2022-07-05 01:16:12 +0100

    first commit

---
+++ javasript.js
@@ -0,0 +1,29 @@
+const ntells = "50years"
+document.write(ntells)
+const sym = Symbol("mary");
+const sym2 = Symbol("mary");
+
+console.log(sym === sym2);
+
+const car = {
+    color: "red",
+    wheel: "four",
+    model: "honda",
+    year: 2005,
+    new: false
+}
+
+console.log(car.model)
+console.log(car)
+
+let car2 = {
+    color: "red",
+    wheel: "four",
+    model: "honda",
+    year: 2005,
+    new: false,
+    seat: "four seats",
+    tinted: "true"
+}
+
+console.log(car2)
